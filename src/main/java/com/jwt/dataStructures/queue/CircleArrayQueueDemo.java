feeb5be40533d3bd80e5422164f6344e94b649ca
jiwt <156529934@qq.com>
2020-04-08 08:57:49 +0800

    使用模拟队列1

---
+++ src/main/java/com/jwt/dataStructures/queue/CircleArrayQueueDemo.java
@@ -0,0 +1,22 @@
+package com.jwt.dataStructures.queue;
+
+/**
+ * 使用环形数组模拟队列
+ */
+public class CircleArrayQueueDemo {
+    public static void main(String[] args) {
+
+    }
+    class CircleArrayQueue{
+        private int maxSize; // 表示数组的最大容量
+        private int front; // 队列头
+        private int rear; // 队列尾
+        private int[] arr; // 该数据用于存放数据, 模拟队列
+        //构造函数
+        public CircleArrayQueue(){
+            front = 0;
+            front = 0;
+
+        }
+    }
+}
